b1775ac0e62e5432987ab7bba942bd8867eba10f
pehvbot <56926228+pehvbot@users.noreply.github.com>
2021-12-10 15:17:50 -0800

    Add RCS and Tanks

---
+++ Source/TestFlightFailureBase_LRRCS.cs
@@ -0,0 +1,18 @@
+﻿using TestFlightAPI;
+
+namespace TestFlight
+{
+
+    public class TestFlightFailureBase_LRRCS : TestFlightFailureBase
+    {
+        protected ModuleRCS rcsfx;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+            this.rcsfx = base.part.FindModuleImplementing<ModuleRCS>();
+        }
+
+    }
+    
+}
